5691b30fc04e01e5739ddf9a07718d8fdacae223
Ariel Costas <arielcostas@mailbox.org>
2022-04-28 01:13:41 +0200

    Implementar gestión de errores diferente para html y json

--- internal/handler/app.go
+++ internal/handler/app.go
@@ -12,13 +12,25 @@ type appError struct {
 	Codigo  int
 }
 
-type appHandler func(w http.ResponseWriter, r *http.Request) *appError
+type htmlAppHandler func(w http.ResponseWriter, r *http.Request) *appError
 
-func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (fn htmlAppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html")
 	if ae := fn(w, r); ae != nil {
 		fmt.Errorf("%s: %s\n", ae.Log, ae.Error.Error())
 		w.WriteHeader(500)
 		w.Write([]byte("<!DOCTYPE html>\n<b>Error!!!</b>\n"))
 		w.Write([]byte(ae.Mensaje))
+	}
+}
+
+type jsonAppHandler func(w http.ResponseWriter, r *http.Request) *appError
+
+func (fn jsonAppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	if ae := fn(w, r); ae != nil {
+		fmt.Errorf("%s: %s\n", ae.Log, ae.Error.Error())
+		w.WriteHeader(500)
+		w.Write([]byte("{error: true, mensaje: \"" + ae.Mensaje + "\"}"))
 	}
 }
--- internal/handler/main.go
+++ internal/handler/main.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -19,10 +20,24 @@ func helloWorld(w http.ResponseWriter, r *http.Request) *appError {
 	return nil
 }
 
+func helloJson(w http.ResponseWriter, r *http.Request) *appError {
+	fuentes, err := fuenteStore.Listar()
+	if err != nil {
+		return &appError{err, "error obtaining fuentes", "Error obteniendo datos", 500}
+	}
+	salida, err := json.MarshalIndent(fuentes, "", "\t")
+	if err != nil {
+		return &appError{err, "error encoding JSON", "Error procesando datos", 500}
+	}
+	w.Write(salida)
+	return nil
+}
+
 func Start(port string) {
 	fuenteStore = model.NewFuenteStore(database.GetDB())
 
-	http.Handle("/", appHandler(helloWorld))
+	http.Handle("/api", jsonAppHandler(helloJson))
+	http.Handle("/", htmlAppHandler(helloWorld))
 
 	fmt.Printf("Iniciando servidor en %s\n", port)
 	err := http.ListenAndServe(port, nil)
