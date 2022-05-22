45867f526d446c77ba543c882f766f912cfe1c48
Ariel Costas <arielcostas@mailbox.org>
2022-05-22 20:02:42 +0200

    Centrar mapa en base a los puntos mostrados

--- static/mapa.js
+++ static/mapa.js
@@ -1,14 +1,21 @@
 var dataElement = document.querySelector("script[type='application/json']");
 var data = JSON.parse(dataElement.textContent);
 
-var map = L.map("map").setView([42.21, -8.7389], 12);
+var map = L.map("map");
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+let markers = [];
+
 data.forEach((fuente) => {
-	L.marker([fuente.Latitud, fuente.Longitud])
+	marker = L.marker([fuente.Latitud, fuente.Longitud])
 		.addTo(map)
 		.bindPopup(`<b>${fuente.Titulo}<br><br><a href="/fuentes/${fuente.Id}">Más información</a>`);
+
+	markers.push(marker);
 });
+
+let group = new L.featureGroup(markers);
+map.fitBounds(group.getBounds().pad(0.035));
